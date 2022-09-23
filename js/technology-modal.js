58882841c382df0f9c9c59ba836c5a3c47632cb1
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-09-23 23:23:13 +0300

    technology modal finished

--- js/technology-modal.js
+++ js/technology-modal.js
@@ -1,25 +1,32 @@
 const refs = {
     container: document.querySelector('.grid'),
+    mobContainer: document.querySelector('.technology__mobile-gallery'),
     technologyBackdrop: document.querySelector('.backdrop'),
     technologyModal: document.querySelector('.technology-modal__content'),
     closeBtn: document.querySelector('.technology-button'),
 };
 
 refs.container.addEventListener('click', onClickModalOpen);
-refs.closeBtn.addEventListener('click', onClickClose);
+refs.mobContainer.addEventListener('click', onClickModalOpen);
+refs.closeBtn.addEventListener('click', onBtnClickClose);
 
 function onClickModalOpen(evt) {
     if (evt.target.nodeName === 'IMG' || evt.target.classList.contains('technology__picture-title')) {
-        refs.technologyBackdrop.classList.toggle('backdrop_is-hidden');
+        refs.technologyBackdrop.classList.remove('backdrop_is-hidden');
     }
 
     refs.technologyModal.insertAdjacentHTML('afterbegin', createModalContent(evt.target));
+
+    window.addEventListener('keydown', onEscClose);
+    refs.technologyBackdrop.addEventListener('click', onBackdropClickClose);
     
 }
 
-function onClickClose() {
-    refs.technologyBackdrop.classList.toggle('backdrop_is-hidden');
+function onBtnClickClose() {
+    refs.technologyBackdrop.classList.add('backdrop_is-hidden');
     refs.technologyModal.innerHTML = '';
+    refs.technologyBackdrop.removeEventListener('click', onBackdropClickClose);
+    window.removeEventListener('keydown', onEscClose);
 }
 
 function createModalContent(element) {
@@ -28,4 +35,24 @@ function createModalContent(element) {
     const image = element.parentNode.querySelector('img');
     return `<h2 class="technology-modal__title">${heading.textContent}</h2><p class="technology-modal__text">${text.textContent}</p><img class="technology-modal__img" src=${image.src} alt=${image.alt}/>`;
 
+}
+
+function onEscClose (evt) {
+    if (evt.key === 'Escape') {
+        refs.technologyBackdrop.classList.add('backdrop_is-hidden');
+        refs.technologyModal.innerHTML = '';
+    }
+
+    window.removeEventListener('keydown', onEscClose);
+    refs.technologyBackdrop.removeEventListener('click', onBackdropClickClose);
+}
+
+function onBackdropClickClose(evt) {
+    if (evt.target.classList.contains('backdrop')) {
+        refs.technologyBackdrop.classList.add('backdrop_is-hidden');
+        refs.technologyModal.innerHTML = '';
+    }
+
+    refs.technologyBackdrop.removeEventListener('click', onBackdropClickClose);
+    window.removeEventListener('keydown', onEscClose);
 }
