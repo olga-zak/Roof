f451286579b7a1cd27aaa5c4387f944d91eaa4bb
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-09-23 15:39:05 +0300

    modal for 1 item done

--- js/technology-modal.js
+++ js/technology-modal.js
@@ -1,6 +1,7 @@
 const refs = {
     container: document.querySelector('.grid'),
-    technologyModal: document.querySelector('.backdrop'),
+    technologyBackdrop: document.querySelector('.backdrop'),
+    technologyModal: document.querySelector('.technology-modal__content'),
     closeBtn: document.querySelector('.technology-button'),
 };
 
@@ -9,11 +10,22 @@ refs.closeBtn.addEventListener('click', onClickClose);
 
 function onClickModalOpen(evt) {
     if (evt.target.nodeName === 'IMG' || evt.target.classList.contains('technology__picture-title')) {
-        refs.technologyModal.classList.toggle('backdrop_is-hidden');
+        refs.technologyBackdrop.classList.toggle('backdrop_is-hidden');
     }
+
+    refs.technologyModal.insertAdjacentHTML('afterbegin', createModalContent(evt.target));
     
 }
 
 function onClickClose() {
-     refs.technologyModal.classList.toggle('backdrop_is-hidden');
+    refs.technologyBackdrop.classList.toggle('backdrop_is-hidden');
+    refs.technologyModal.innerHTML = '';
+}
+
+function createModalContent(element) {
+    const heading = element.parentNode.querySelector('.technology__picture-title');
+    const text = element.parentNode.querySelector('.technology__description');
+    const image = element.parentNode.querySelector('img');
+    return `<h2 class="technology-modal__title">${heading.textContent}</h2><p class="technology-modal__text">${text.textContent}</p><img class="technology-modal__img" src=${image.src} alt=${image.alt}/>`;
+
 }
