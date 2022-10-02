eef6766d187aab7fd1d0cbf2322da52b345af547
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-10-02 21:23:31 +0300

    Update order-form-open.js

--- src/js/order-form-open.js
+++ src/js/order-form-open.js
@@ -4,19 +4,33 @@ const refs = refsList();
 
 refs.heroBtn.addEventListener('click', onHeroBtnClickModalOpen);
 refs.orderBtn.addEventListener('click', onOrderBtnClickModalClose);
+window.addEventListener('scroll', () => {
+  document.documentElement.style.setProperty(
+    '--scroll-y',
+    `${window.scrollY}px`
+  );
+});
 
 function onHeroBtnClickModalOpen() {
   refs.orderBackdrop.classList.remove('backdrop_is-hidden');
-  document.body.style.position = 'fixed';
-  document.body.style.width = '100%';
+
+  const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
+  const body = document.body;
+  body.style.position = 'fixed';
+  body.style.top = `-${scrollY}`;
 
   window.addEventListener('keydown', onEscClose);
   refs.orderBackdrop.addEventListener('click', onBackdropClickClose);
 }
 
 function onOrderBtnClickModalClose() {
+  const body = document.body;
+  const scrollY = body.style.top;
+  body.style.position = '';
+  body.style.top = '';
+  window.scrollTo(0, parseInt(scrollY || '0') * -1);
+
   refs.orderBackdrop.classList.add('backdrop_is-hidden');
-  document.body.style.position = '';
 
   window.removeEventListener('keydown', onEscClose);
   refs.orderBackdrop.removeEventListener('click', onBackdropClickClose);
@@ -25,7 +39,11 @@ function onOrderBtnClickModalClose() {
 function onEscClose(evt) {
   if (evt.key === 'Escape')
     refs.orderBackdrop.classList.add('backdrop_is-hidden');
-  document.body.style.position = '';
+  const body = document.body;
+  const scrollY = body.style.top;
+  body.style.position = '';
+  body.style.top = '';
+  window.scrollTo(0, parseInt(scrollY || '0') * -1);
   window.removeEventListener('keydown', onEscClose);
   refs.orderBackdrop.removeEventListener('click', onBackdropClickClose);
 }
@@ -33,7 +51,11 @@ function onEscClose(evt) {
 function onBackdropClickClose(evt) {
   if (!evt.target.classList.contains('backdrop')) return;
   refs.orderBackdrop.classList.add('backdrop_is-hidden');
-  document.body.style.position = '';
+  const body = document.body;
+  const scrollY = body.style.top;
+  body.style.position = '';
+  body.style.top = '';
+  window.scrollTo(0, parseInt(scrollY || '0') * -1);
   refs.orderBackdrop.removeEventListener('click', onBackdropClickClose);
   window.removeEventListener('keydown', onEscClose);
 }
