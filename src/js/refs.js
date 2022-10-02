5b91ef65a5bf8a3c8d2b5db2e0c2f7c333c7aacd
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-10-02 16:25:17 +0300

    script for all refs added

---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+export default function refsList() {
+    return {
+        heroBtn: document.querySelector('.hero__button'),
+        orderBackdrop: document.querySelector('.order__backdrop'),
+        orderBtn: document.querySelector('.order__button'),
+    }
+}
